968286577caea38cfc18641010f37954fff1f5a2
ErhanTolek <erhantolek2207@gmail.com>
2025-05-20 21:52:55 +0300

    erh-pkg

--- sort/sort.go
+++ sort/sort.go
@@ -3,37 +3,22 @@ package sort
 // Sort is a generic struct for sorting slices of maps.
 // It uses a less function to determine the order of elements.
 // K is the type of the key in the map, and V is the type of the value.
-type Sort[K comparable, V comparable] struct {
-	Less func(i, j V) bool
+type Sort[T any] struct {
+	Less func(a, b T) bool
 }
 
-type SortingClient[K comparable, V comparable] interface {
-	BubbleSort(arr []map[K]V, key K, less func(a, b V) bool) []map[K]V
+func NewSort[T any](less func(a, b T) bool) *Sort[T] {
+	return &Sort[T]{Less: less}
 }
 
-// NewSort creates a new Sort instance with the provided less function.
-// Less function should decide the order of elements.
-func NewSort[K comparable, V comparable](less func(i, j V) bool) *Sort[K, V] {
-	return &Sort[K, V]{
-		Less: less,
-	}
-}
-
-func (sort Sort[K, V]) BubbleSort(arr []map[K]V, key K) []map[K]V {
+func (s *Sort[T]) BubbleSort(arr []T) []T {
 	n := len(arr)
-	sorted := make([]map[K]V, n)
+	sorted := make([]T, n)
 	copy(sorted, arr)
 
 	for i := 0; i < n-1; i++ {
 		for j := 0; j < n-i-1; j++ {
-			v1, ok1 := sorted[j][key]
-			v2, ok2 := sorted[j+1][key]
-
-			if !ok1 || !ok2 {
-				continue
-			}
-
-			if sort.Less(v2, v1) {
+			if s.Less(sorted[j+1], sorted[j]) {
 				sorted[j], sorted[j+1] = sorted[j+1], sorted[j]
 			}
 		}
